d697e68a67e4e5d2cb7fbbbfffe697acd91e0c03
Ryan <ryan.maclear@gmail.com>
2023-05-16 20:22:16 +0200

    Use PgPool and first pass of successful tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Faield to read configuration file.");
-    let connection = PgConnection::connect(
+    let connection_pool = PgPool::connect(
         &configuration.database.connection_string()
     )
     .await
@@ -15,5 +15,5 @@ async fn main() -> std::io::Result<()> {
     // We removed the hard coded 8000 - it's now coming from our settings
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
